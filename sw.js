bbae4194e263bd41e9c01c0dde431ada45e1ba8a
martinezhaydn-byte <martinezhaydn@gmail.com>
2025-08-27 19:06:53 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
 
-const CACHE_NAME = 'dance-academy-cache-v5';
-const ASSETS = [
-  './','./index.html','./styles.css','./app.js','./manifest.json',
-  './icons/icon-192.png','./icons/icon-512.png'
-];
+const CACHE_NAME = 'academia-nh-cache-v1';
+const ASSETS = ['./','./index.html','./styles.css','./manifest.json',
+'./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
